85429c199de9b5184fb811457a1332b834391c4f
iamfedoroff <iamfedoroff@gmail.com>
2023-03-29 09:53:27 +0300

    Box PML for 3D instead of cylinder

--- src/pml.jl
+++ src/pml.jl
@@ -86,39 +86,21 @@ function pml(grid::Grid3D, box)
     sz = zeros(Nx, Ny, Nz)
 
     # box:
-    # xb1, xb2 = xmin + xpml1, xmax - xpml2
-    # yb1, yb2 = ymin + ypml1, ymax - ypml2
-    # zb1, zb2 = zmin + zpml1, zmax - zpml2
-    # for iz=1:Nz, iy=1:Ny, ix=1:Nx
-    #     if x[ix] < xb1
-    #         sx[ix,iy,iz] = abs((x[ix] - xb1) / xpml1)^3
-    #     end
-    #     if x[ix] > xb2
-    #         sx[ix,iy,iz] = abs((x[ix] - xb2) / xpml2)^3
-    #     end
-    #     if y[iy] < yb1
-    #         sy[ix,iy,iz] = abs((y[iy] - yb1) / ypml1)^3
-    #     end
-    #     if y[iy] > yb2
-    #         sy[ix,iy,iz] = abs((y[iy] - yb2) / ypml2)^3
-    #     end
-    #     if z[iz] < zb1
-    #         sz[ix,iy,iz] = abs((z[iz] - zb1) / zpml1)^3
-    #     end
-    #     if z[iz] > zb2
-    #         sz[ix,iy,iz] = abs((z[iz] - zb2) / zpml2)^3
-    #     end
-    # end
-
-    # z cylinder:
-    rad = xmax - xpml1
-    rmax = xmax
+    xb1, xb2 = xmin + xpml1, xmax - xpml2
+    yb1, yb2 = ymin + ypml1, ymax - ypml2
     zb1, zb2 = zmin + zpml1, zmax - zpml2
     for iz=1:Nz, iy=1:Ny, ix=1:Nx
-        r = sqrt(x[ix]^2 + y[iy]^2)
-        if r > rad
-            sx[ix,iy,iz] = abs((r - rad) / (rmax - rad))^3
-            sy[ix,iy,iz] = abs((r - rad) / (rmax - rad))^3
+        if x[ix] < xb1
+            sx[ix,iy,iz] = abs((x[ix] - xb1) / xpml1)^3
+        end
+        if x[ix] > xb2
+            sx[ix,iy,iz] = abs((x[ix] - xb2) / xpml2)^3
+        end
+        if y[iy] < yb1
+            sy[ix,iy,iz] = abs((y[iy] - yb1) / ypml1)^3
+        end
+        if y[iy] > yb2
+            sy[ix,iy,iz] = abs((y[iy] - yb2) / ypml2)^3
         end
         if z[iz] < zb1
             sz[ix,iy,iz] = abs((z[iz] - zb1) / zpml1)^3
@@ -127,6 +109,24 @@ function pml(grid::Grid3D, box)
             sz[ix,iy,iz] = abs((z[iz] - zb2) / zpml2)^3
         end
     end
+
+    # z cylinder:
+    # rad = xmax - xpml1
+    # rmax = xmax
+    # zb1, zb2 = zmin + zpml1, zmax - zpml2
+    # for iz=1:Nz, iy=1:Ny, ix=1:Nx
+    #     r = sqrt(x[ix]^2 + y[iy]^2)
+    #     if r > rad
+    #         sx[ix,iy,iz] = abs((r - rad) / (rmax - rad))^3
+    #         sy[ix,iy,iz] = abs((r - rad) / (rmax - rad))^3
+    #     end
+    #     if z[iz] < zb1
+    #         sz[ix,iy,iz] = abs((z[iz] - zb1) / zpml1)^3
+    #     end
+    #     if z[iz] > zb2
+    #         sz[ix,iy,iz] = abs((z[iz] - zb2) / zpml2)^3
+    #     end
+    # end
 
     return sx, sy, sz
 end
