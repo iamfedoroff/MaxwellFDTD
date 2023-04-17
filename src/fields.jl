21916eb6cbab638857ab003a300ee2bcc13051c9
iamfedoroff <iamfedoroff@gmail.com>
2023-04-17 16:13:31 +0300

    CPML for 3D

--- src/fields.jl
+++ src/fields.jl
@@ -69,8 +69,8 @@ struct Field2D{G, T, A} <: Field
     dExz :: A
     dEzx :: A
     # magnetic field derivatives:
-    dHyz :: A
     dHyx :: A
+    dHyz :: A
 end
 
 @adapt_structure Field2D
@@ -78,8 +78,8 @@ end
 
 function Field(grid::Grid2D; w0)
     (; Nx, Nz) = grid
-    Hy, Ex, Ez, dExz, dEzx, dHyz, dHyx = (zeros(Nx,Nz) for i=1:7)
-    return Field2D(grid, w0, Hy, Ex, Ez, dExz, dEzx, dHyz, dHyx)
+    Hy, Ex, Ez, dExz, dEzx, dHyx, dHyz = (zeros(Nx,Nz) for i=1:7)
+    return Field2D(grid, w0, Hy, Ex, Ez, dExz, dEzx, dHyx, dHyz)
 end
 
 
@@ -137,10 +137,16 @@ function derivatives_E_kernel!(field::Field2D)
 end
 
 
-
 function derivatives_H!(field::Field2D)
-    (; grid, Hy, dHyz, dHyx) = field
+    (; grid, Hy, dHyx, dHyz) = field
     (; Nx, Nz, dx, dz) = grid
+
+    for iz=1:Nz
+        dHyx[1,iz] = (Hy[1,iz] - Hy[Nx,iz]) / dx
+        for ix=2:Nx
+            dHyx[ix,iz] = (Hy[ix,iz] - Hy[ix-1,iz]) / dx
+        end
+    end
 
     for ix=1:Nx
         dHyz[ix,1] = (Hy[ix,1] - Hy[ix,Nz]) / dz
@@ -148,13 +154,6 @@ function derivatives_H!(field::Field2D)
     for iz=2:Nz
         for ix=1:Nx
             dHyz[ix,iz] = (Hy[ix,iz] - Hy[ix,iz-1]) / dz
-        end
-    end
-
-    for iz=1:Nz
-        dHyx[1,iz] = (Hy[1,iz] - Hy[Nx,iz]) / dx
-        for ix=2:Nx
-            dHyx[ix,iz] = (Hy[ix,iz] - Hy[ix-1,iz]) / dx
         end
     end
 
@@ -184,8 +183,8 @@ function derivatives_H_kernel!(field::Field2D)
         elseif iz == 1
             dHyz[ix,1] = (Hy[ix,1] - Hy[ix,Nz]) / dz
         else
-            dHyz[ix,iz] = (Hy[ix,iz] - Hy[ix,iz-1]) / dz
             dHyx[ix,iz] = (Hy[ix,iz] - Hy[ix-1,iz]) / dx
+            dHyz[ix,iz] = (Hy[ix,iz] - Hy[ix,iz-1]) / dz
         end
     end
     return nothing
@@ -212,14 +211,20 @@ struct Field3D{G, T, A} <: Field
     Ex :: A
     Ey :: A
     Ez :: A
-    # electric field curl:
-    CEx :: A
-    CEy :: A
-    CEz :: A
-    # magnetic field curl:
-    CHx :: A
-    CHy :: A
-    CHz :: A
+    # electric field derivatives:
+    dExy :: A
+    dExz :: A
+    dEyx :: A
+    dEyz :: A
+    dEzx :: A
+    dEzy :: A
+    # magnetic field derivatives:
+    dHxy :: A
+    dHxz :: A
+    dHyx :: A
+    dHyz :: A
+    dHzx :: A
+    dHzy :: A
 end
 
 @adapt_structure Field3D
@@ -228,94 +233,52 @@ end
 function Field(grid::Grid3D; w0)
     (; Nx, Ny, Nz) = grid
     Hx, Hy, Hz, Ex, Ey, Ez = (zeros(Nx,Ny,Nz) for i=1:6)
-    CEx, CEy, CEz, CHx, CHy, CHz = (zeros(Nx,Ny,Nz) for i=1:6)
+    dExy, dExz, dEyx, dEyz, dEzx, dEzy = (zeros(Nx,Ny,Nz) for i=1:6)
+    dHxy, dHxz, dHyx, dHyz, dHzx, dHzy = (zeros(Nx,Ny,Nz) for i=1:6)
     return Field3D(
-        grid, w0, Hx, Hy, Hz, Ex, Ey, Ez, CEx, CEy, CEz, CHx, CHy, CHz,
+        grid, w0, Hx, Hy, Hz, Ex, Ey, Ez, dExy, dExz, dEyx, dEyz, dEzx, dEzy,
+        dHxy, dHxz, dHyx, dHyz, dHzx, dHzy,
     )
 end
 
 
-function curl_E!(field::Field3D)
-    (; grid, Ex, Ey, Ez, CEx, CEy, CEz) = field
+function derivatives_E!(field::Field3D)
+    (; grid, Ex, Ey, Ez, dExy, dExz, dEyx, dEyz, dEzx, dEzy) = field
     (; Nx, Ny, Nz, dx, dy, dz) = grid
-
-    # curl CEx:
-    for iz=1:Nz-1
-        for iy=1:Ny-1
-            for ix=1:Nx
-                CEx[ix,iy,iz] = (Ez[ix,iy+1,iz] - Ez[ix,iy,iz]) / dy -
-                                (Ey[ix,iy,iz+1] - Ey[ix,iy,iz]) / dz
-            end
-        end
-        for ix=1:Nx
-            CEx[ix,Ny,iz] = (Ez[ix,1,iz] - Ez[ix,Ny,iz]) / dy -
-                            (Ey[ix,Ny,iz+1] - Ey[ix,Ny,iz]) / dz
-        end
-    end
-    for iy=1:Ny-1
-        for ix=1:Nx
-            CEx[ix,iy,Nz] = (Ez[ix,iy+1,Nz] - Ez[ix,iy,Nz]) / dy -
-                            (Ey[ix,iy,1] - Ey[ix,iy,Nz]) / dz
-        end
-    end
-    for ix=1:Nx
-        CEx[ix,Ny,Nz] = (Ez[ix,1,Nz] - Ez[ix,Ny,Nz]) / dy -
-                        (Ey[ix,Ny,1] - Ey[ix,Ny,Nz]) / dz
-    end
-
-    # curl CEy:
-    for iz=1:Nz-1
-        for iy=1:Ny
-            for ix=1:Nx-1
-                CEy[ix,iy,iz] = (Ex[ix,iy,iz+1] - Ex[ix,iy,iz]) / dz -
-                                (Ez[ix+1,iy,iz] - Ez[ix,iy,iz]) / dx
-            end
-            CEy[Nx,iy,iz] = (Ex[Nx,iy,iz+1] - Ex[Nx,iy,iz]) / dz -
-                            (Ez[1,iy,iz] - Ez[Nx,iy,iz]) / dx
+    for iz=1:Nz, iy=1:Ny, ix=1:Nx
+        if ix == Nx
+            dEyx[Nx,iy,iz] = (Ey[1,iy,iz] - Ey[Nx,iy,iz]) / dx
+            dEzx[Nx,iy,iz] = (Ez[1,iy,iz] - Ez[Nx,iy,iz]) / dx
+        elseif iy == Ny
+            dExy[ix,Ny,iz] = (Ex[ix,1,iz] - Ex[ix,Ny,iz]) / dy
+            dEzy[ix,Ny,iz] = (Ez[ix,1,iz] - Ez[ix,Ny,iz]) / dy
+        elseif iz == Nz
+            dExz[ix,iy,Nz] = (Ex[ix,iy,1] - Ex[ix,iy,Nz]) / dz
+            dEyz[ix,iy,Nz] = (Ey[ix,iy,1] - Ey[ix,iy,Nz]) / dz
+        else
+            dExy[ix,iy,iz] = (Ex[ix,iy+1,iz] - Ex[ix,iy,iz]) / dy
+            dExz[ix,iy,iz] = (Ex[ix,iy,iz+1] - Ex[ix,iy,iz]) / dz
+            dEyx[ix,iy,iz] = (Ey[ix+1,iy,iz] - Ey[ix,iy,iz]) / dx
+            dEyz[ix,iy,iz] = (Ey[ix,iy,iz+1] - Ey[ix,iy,iz]) / dz
+            dEzx[ix,iy,iz] = (Ez[ix+1,iy,iz] - Ez[ix,iy,iz]) / dx
+            dEzy[ix,iy,iz] = (Ez[ix,iy+1,iz] - Ez[ix,iy,iz]) / dy
         end
     end
-    for iy=1:Ny
-        for ix=1:Nx-1
-            CEy[ix,iy,Nz] = (Ex[ix,iy,1] - Ex[ix,iy,Nz]) / dz -
-                            (Ez[ix+1,iy,Nz] - Ez[ix,iy,Nz]) / dx
-        end
-        CEy[Nx,iy,Nz] = (Ex[Nx,iy,1] - Ex[Nx,iy,Nz]) / dz -
-                        (Ez[1,iy,Nz] - Ez[Nx,iy,Nz]) / dx
-    end
-
-    # curl CEz:
-    for iz=1:Nz
-        for iy=1:Ny-1
-            for ix=1:Nx-1
-                CEz[ix,iy,iz] = (Ey[ix+1,iy,iz] - Ey[ix,iy,iz]) / dx -
-                                (Ex[ix,iy+1,iz] - Ex[ix,iy,iz]) / dy
-            end
-            CEz[Nx,iy,iz] = (Ey[1,iy,iz] - Ey[Nx,iy,iz]) / dx -
-                            (Ex[Nx,iy+1,iz] - Ex[Nx,iy,iz]) / dy
-        end
-        for ix=1:Nx-1
-            CEz[ix,Ny,iz] = (Ey[ix+1,Ny,iz] - Ey[ix,Ny,iz]) / dx -
-                            (Ex[ix,1,iz] - Ex[ix,Ny,iz]) / dy
-        end
-        CEz[Nx,Ny,iz] = (Ey[1,Ny,iz] - Ey[Nx,Ny,iz]) / dx -
-                        (Ex[Nx,1,iz] - Ex[Nx,Ny,iz]) / dy
-    end
-
     return nothing
 end
 
 
-function curl_E!(field::Field3D{G,T,A}) where {G,T,A<:CuArray}
+function derivatives_E!(field::Field3D{G,T,A}) where {G,T,A<:CuArray}
     (; Ex) = field
     N = length(Ex)
-    @krun N curl_E_kernel!(field)
+    @krun N derivatives_E_kenel!(field)
     return nothing
 end
-function curl_E_kernel!(field::Field3D)
+function derivatives_E_kenel!(field::Field3D)
     id = (blockIdx().x - 1) * blockDim().x + threadIdx().x
     stride = blockDim().x * gridDim().x
 
-    (; grid, Ex, Ey, Ez, CEx, CEy, CEz) = field
+    (; grid, Ex, Ey, Ez, dExy, dExz, dEyx, dEyz, dEzx, dEzy) = field
     (; Nx, Ny, Nz, dx, dy, dz) = grid
 
     ci = CartesianIndices(Ex)
@@ -323,138 +286,65 @@ function curl_E_kernel!(field::Field3D)
         ix = ci[ici][1]
         iy = ci[ici][2]
         iz = ci[ici][3]
-
-        # curl CEx:
-        if (iy == Ny) && (iz == Nz)
-            CEx[ix,Ny,Nz] = (Ez[ix,1,Nz] - Ez[ix,Ny,Nz]) / dy -
-                            (Ey[ix,Ny,1] - Ey[ix,Ny,Nz]) / dz
+        if ix == Nx
+            dEyx[Nx,iy,iz] = (Ey[1,iy,iz] - Ey[Nx,iy,iz]) / dx
+            dEzx[Nx,iy,iz] = (Ez[1,iy,iz] - Ez[Nx,iy,iz]) / dx
         elseif iy == Ny
-            CEx[ix,Ny,iz] = (Ez[ix,1,iz] - Ez[ix,Ny,iz]) / dy -
-                            (Ey[ix,Ny,iz+1] - Ey[ix,Ny,iz]) / dz
+            dExy[ix,Ny,iz] = (Ex[ix,1,iz] - Ex[ix,Ny,iz]) / dy
+            dEzy[ix,Ny,iz] = (Ez[ix,1,iz] - Ez[ix,Ny,iz]) / dy
         elseif iz == Nz
-            CEx[ix,iy,Nz] = (Ez[ix,iy+1,Nz] - Ez[ix,iy,Nz]) / dy -
-                            (Ey[ix,iy,1] - Ey[ix,iy,Nz]) / dz
+            dExz[ix,iy,Nz] = (Ex[ix,iy,1] - Ex[ix,iy,Nz]) / dz
+            dEyz[ix,iy,Nz] = (Ey[ix,iy,1] - Ey[ix,iy,Nz]) / dz
         else
-            CEx[ix,iy,iz] = (Ez[ix,iy+1,iz] - Ez[ix,iy,iz]) / dy -
-                            (Ey[ix,iy,iz+1] - Ey[ix,iy,iz]) / dz
-        end
-
-        # curl CEy:
-        if (ix == Nx) && (iz == Nz)
-            CEy[Nx,iy,Nz] = (Ex[Nx,iy,1] - Ex[Nx,iy,Nz]) / dz -
-                            (Ez[1,iy,Nz] - Ez[Nx,iy,Nz]) / dx
-        elseif ix == Nx
-            CEy[Nx,iy,iz] = (Ex[Nx,iy,iz+1] - Ex[Nx,iy,iz]) / dz -
-                            (Ez[1,iy,iz] - Ez[Nx,iy,iz]) / dx
-        elseif iz == Nz
-            CEy[ix,iy,Nz] = (Ex[ix,iy,1] - Ex[ix,iy,Nz]) / dz -
-                            (Ez[ix+1,iy,Nz] - Ez[ix,iy,Nz]) / dx
-        else
-            CEy[ix,iy,iz] = (Ex[ix,iy,iz+1] - Ex[ix,iy,iz]) / dz -
-                            (Ez[ix+1,iy,iz] - Ez[ix,iy,iz]) / dx
-        end
-
-        # curl CEz:
-        if (ix == Nx) && (iy == Ny)
-            CEz[Nx,Ny,iz] = (Ey[1,Ny,iz] - Ey[Nx,Ny,iz]) / dx -
-                            (Ex[Nx,1,iz] - Ex[Nx,Ny,iz]) / dy
-        elseif ix == Nx
-            CEz[Nx,iy,iz] = (Ey[1,iy,iz] - Ey[Nx,iy,iz]) / dx -
-                            (Ex[Nx,iy+1,iz] - Ex[Nx,iy,iz]) / dy
-        elseif iy == Ny
-            CEz[ix,Ny,iz] = (Ey[ix+1,Ny,iz] - Ey[ix,Ny,iz]) / dx -
-                            (Ex[ix,1,iz] - Ex[ix,Ny,iz]) / dy
-        else
-            CEz[ix,iy,iz] = (Ey[ix+1,iy,iz] - Ey[ix,iy,iz]) / dx -
-                            (Ex[ix,iy+1,iz] - Ex[ix,iy,iz]) / dy
+            dExy[ix,iy,iz] = (Ex[ix,iy+1,iz] - Ex[ix,iy,iz]) / dy
+            dExz[ix,iy,iz] = (Ex[ix,iy,iz+1] - Ex[ix,iy,iz]) / dz
+            dEyx[ix,iy,iz] = (Ey[ix+1,iy,iz] - Ey[ix,iy,iz]) / dx
+            dEyz[ix,iy,iz] = (Ey[ix,iy,iz+1] - Ey[ix,iy,iz]) / dz
+            dEzx[ix,iy,iz] = (Ez[ix+1,iy,iz] - Ez[ix,iy,iz]) / dx
+            dEzy[ix,iy,iz] = (Ez[ix,iy+1,iz] - Ez[ix,iy,iz]) / dy
         end
     end
-
     return nothing
 end
 
 
-function curl_H!(field::Field3D)
-    (; grid, Hx, Hy, Hz, CHx, CHy, CHz) = field
+function derivatives_H!(field::Field3D)
+    (; grid, Hx, Hy, Hz, dHxy, dHxz, dHyx, dHyz, dHzx, dHzy) = field
     (; Nx, Ny, Nz, dx, dy, dz) = grid
-
-    # curl CHx:
-    for ix=1:Nx
-        CHx[ix,1,1] = (Hz[ix,1,1] - Hz[ix,Ny,1]) / dy -
-                      (Hy[ix,1,1] - Hy[ix,1,Nz]) / dz
-    end
-    for iy=2:Ny
-        for ix=1:Nx
-            CHx[ix,iy,1] = (Hz[ix,iy,1] - Hz[ix,iy-1,1]) / dy -
-                           (Hy[ix,iy,1] - Hy[ix,iy,Nz]) / dz
+    for iz=1:Nz, iy=1:Ny, ix=1:Nx
+        if ix == 1
+            dHyx[1,iy,iz] = (Hy[1,iy,iz] - Hy[Nx,iy,iz]) / dx
+            dHzx[1,iy,iz] = (Hz[1,iy,iz] - Hz[Nx,iy,iz]) / dx
+        elseif iy == 1
+            dHxy[ix,1,iz] = (Hx[ix,1,iz] - Hx[ix,Ny,iz]) / dy
+            dHzy[ix,1,iz] = (Hz[ix,1,iz] - Hz[ix,Ny,iz]) / dy
+        elseif iz == 1
+            dHxz[ix,iy,1] = (Hx[ix,iy,1] - Hx[ix,iy,Nz]) / dz
+            dHyz[ix,iy,1] = (Hy[ix,iy,1] - Hy[ix,iy,Nz]) / dz
+        else
+            dHxy[ix,iy,iz] = (Hx[ix,iy,iz] - Hx[ix,iy-1,iz]) / dy
+            dHxz[ix,iy,iz] = (Hx[ix,iy,iz] - Hx[ix,iy,iz-1]) / dz
+            dHyx[ix,iy,iz] = (Hy[ix,iy,iz] - Hy[ix-1,iy,iz]) / dx
+            dHyz[ix,iy,iz] = (Hy[ix,iy,iz] - Hy[ix,iy,iz-1]) / dz
+            dHzx[ix,iy,iz] = (Hz[ix,iy,iz] - Hz[ix-1,iy,iz]) / dx
+            dHzy[ix,iy,iz] = (Hz[ix,iy,iz] - Hz[ix,iy-1,iz]) / dy
         end
     end
-    for iz=2:Nz
-        for ix=1:Nx
-            CHx[ix,1,iz] = (Hz[ix,1,iz] - Hz[ix,Ny,iz]) / dy -
-                           (Hy[ix,1,iz] - Hy[ix,1,iz-1]) / dz
-        end
-        for iy=2:Ny
-            for ix=1:Nx
-                CHx[ix,iy,iz] = (Hz[ix,iy,iz] - Hz[ix,iy-1,iz]) / dy -
-                                (Hy[ix,iy,iz] - Hy[ix,iy,iz-1]) / dz
-            end
-        end
-    end
-
-    # curl CHy:
-    for iy=1:Ny
-        CHy[1,iy,1] = (Hx[1,iy,1] - Hx[1,iy,Nz]) / dz -
-                      (Hz[1,iy,1] - Hz[Nx,iy,1]) / dx
-        for ix=2:Nx
-            CHy[ix,iy,1] = (Hx[ix,iy,1] - Hx[ix,iy,Nz]) / dz -
-                           (Hz[ix,iy,1] - Hz[ix-1,iy,1]) / dx
-        end
-    end
-    for iz=2:Nz
-        for iy=1:Ny
-            CHy[1,iy,iz] = (Hx[1,iy,iz] - Hx[1,iy,iz-1]) / dz -
-                           (Hz[1,iy,iz] - Hz[Nx,iy,iz]) / dx
-            for ix=2:Nx
-                CHy[ix,iy,iz] = (Hx[ix,iy,iz] - Hx[ix,iy,iz-1]) / dz -
-                                (Hz[ix,iy,iz] - Hz[ix-1,iy,iz]) / dx
-            end
-        end
-    end
-
-    # curl CHz:
-    for iz=1:Nz
-        CHz[1,1,iz] = (Hy[1,1,iz] - Hy[Nx,1,iz]) / dx -
-                      (Hx[1,1,iz] - Hx[1,Ny,iz]) / dy
-        for ix=2:Nx
-            CHz[ix,1,iz] = (Hy[ix,1,iz] - Hy[ix-1,1,iz]) / dx -
-                           (Hx[ix,1,iz] - Hx[ix,Ny,iz]) / dy
-        end
-        for iy=2:Ny
-            CHz[1,iy,iz] = (Hy[1,iy,iz] - Hy[Nx,iy,iz]) / dx -
-                           (Hx[1,iy,iz] - Hx[1,iy-1,iz]) / dy
-            for ix=2:Nx
-                CHz[ix,iy,iz] = (Hy[ix,iy,iz] - Hy[ix-1,iy,iz]) / dx -
-                                (Hx[ix,iy,iz] - Hx[ix,iy-1,iz]) / dy
-            end
-        end
-    end
-
     return nothing
 end
 
 
-function curl_H!(field::Field3D{G,T,A}) where {G,T,A<:CuArray}
+function derivatives_H!(field::Field3D{G,T,A}) where {G,T,A<:CuArray}
     (; Hx) = field
     N = length(Hx)
-    @krun N curl_H_kernel!(field)
+    @krun N derivatives_H_kernel!(field)
     return nothing
 end
-function curl_H_kernel!(field::Field3D)
+function derivatives_H_kernel!(field::Field3D)
     id = (blockIdx().x - 1) * blockDim().x + threadIdx().x
     stride = blockDim().x * gridDim().x
 
-    (; grid, Hx, Hy, Hz, CHx, CHy, CHz) = field
+    (; grid, Hx, Hy, Hz, dHxy, dHxz, dHyx, dHyz, dHzx, dHzy) = field
     (; Nx, Ny, Nz, dx, dy, dz) = grid
 
     ci = CartesianIndices(Hx)
@@ -462,53 +352,24 @@ function curl_H_kernel!(field::Field3D)
         ix = ci[ici][1]
         iy = ci[ici][2]
         iz = ci[ici][3]
-
-        # curl CHx:
-        if (iy == 1) && (iz == 1)
-            CHx[ix,1,1] = (Hz[ix,1,1] - Hz[ix,Ny,1]) / dy -
-                          (Hy[ix,1,1] - Hy[ix,1,Nz]) / dz
+        if ix == 1
+            dHyx[1,iy,iz] = (Hy[1,iy,iz] - Hy[Nx,iy,iz]) / dx
+            dHzx[1,iy,iz] = (Hz[1,iy,iz] - Hz[Nx,iy,iz]) / dx
         elseif iy == 1
-            CHx[ix,1,iz] = (Hz[ix,1,iz] - Hz[ix,Ny,iz]) / dy -
-                           (Hy[ix,1,iz] - Hy[ix,1,iz-1]) / dz
+            dHxy[ix,1,iz] = (Hx[ix,1,iz] - Hx[ix,Ny,iz]) / dy
+            dHzy[ix,1,iz] = (Hz[ix,1,iz] - Hz[ix,Ny,iz]) / dy
         elseif iz == 1
-            CHx[ix,iy,1] = (Hz[ix,iy,1] - Hz[ix,iy-1,1]) / dy -
-                           (Hy[ix,iy,1] - Hy[ix,iy,Nz]) / dz
+            dHxz[ix,iy,1] = (Hx[ix,iy,1] - Hx[ix,iy,Nz]) / dz
+            dHyz[ix,iy,1] = (Hy[ix,iy,1] - Hy[ix,iy,Nz]) / dz
         else
-            CHx[ix,iy,iz] = (Hz[ix,iy,iz] - Hz[ix,iy-1,iz]) / dy -
-                            (Hy[ix,iy,iz] - Hy[ix,iy,iz-1]) / dz
-        end
-
-        # curl CHy:
-        if (ix == 1) && (iz == 1)
-            CHy[1,iy,1] = (Hx[1,iy,1] - Hx[1,iy,Nz]) / dz -
-                          (Hz[1,iy,1] - Hz[Nx,iy,1]) / dx
-        elseif ix == 1
-            CHy[1,iy,iz] = (Hx[1,iy,iz] - Hx[1,iy,iz-1]) / dz -
-                           (Hz[1,iy,iz] - Hz[Nx,iy,iz]) / dx
-        elseif iz == 1
-            CHy[ix,iy,1] = (Hx[ix,iy,1] - Hx[ix,iy,Nz]) / dz -
-                           (Hz[ix,iy,1] - Hz[ix-1,iy,1]) / dx
-        else
-            CHy[ix,iy,iz] = (Hx[ix,iy,iz] - Hx[ix,iy,iz-1]) / dz -
-                            (Hz[ix,iy,iz] - Hz[ix-1,iy,iz]) / dx
-        end
-
-        # curl CHz:
-        if (ix == 1) && (iy == 1)
-            CHz[1,1,iz] = (Hy[1,1,iz] - Hy[Nx,1,iz]) / dx -
-                          (Hx[1,1,iz] - Hx[1,Ny,iz]) / dy
-        elseif ix == 1
-            CHz[1,iy,iz] = (Hy[1,iy,iz] - Hy[Nx,iy,iz]) / dx -
-                           (Hx[1,iy,iz] - Hx[1,iy-1,iz]) / dy
-        elseif iy == 1
-            CHz[ix,1,iz] = (Hy[ix,1,iz] - Hy[ix-1,1,iz]) / dx -
-                           (Hx[ix,1,iz] - Hx[ix,Ny,iz]) / dy
-        else
-            CHz[ix,iy,iz] = (Hy[ix,iy,iz] - Hy[ix-1,iy,iz]) / dx -
-                            (Hx[ix,iy,iz] - Hx[ix,iy-1,iz]) / dy
+            dHxy[ix,iy,iz] = (Hx[ix,iy,iz] - Hx[ix,iy-1,iz]) / dy
+            dHxz[ix,iy,iz] = (Hx[ix,iy,iz] - Hx[ix,iy,iz-1]) / dz
+            dHyx[ix,iy,iz] = (Hy[ix,iy,iz] - Hy[ix-1,iy,iz]) / dx
+            dHyz[ix,iy,iz] = (Hy[ix,iy,iz] - Hy[ix,iy,iz-1]) / dz
+            dHzx[ix,iy,iz] = (Hz[ix,iy,iz] - Hz[ix-1,iy,iz]) / dx
+            dHzy[ix,iy,iz] = (Hz[ix,iy,iz] - Hz[ix,iy-1,iz]) / dy
         end
     end
-
     return nothing
 end
 
--- src/models.jl
+++ src/models.jl
@@ -171,8 +171,8 @@ struct Model2D{F, S, T, R, A1, A2} <: Model
     Bz :: A1
     psiExz :: A2
     psiEzx :: A2
-    psiHyz :: A2
     psiHyx :: A2
+    psiHyz :: A2
 end
 
 @adapt_structure Model2D
@@ -200,11 +200,11 @@ function Model(
     Mh = dt / MU0
     Me = dt / EPS0
 
-    psiExz, psiEzx, psiHyz, psiHyx = (zeros(Nx,Nz) for i=1:4)
+    psiExz, psiEzx, psiHyx, psiHyz = (zeros(Nx,Nz) for i=1:4)
 
     return Model2D(
         field, source, Nt, dt, t, Mh, Me, Kx, Ax, Bx, Kz, Az, Bz,
-        psiExz, psiEzx, psiHyz, psiHyx,
+        psiExz, psiEzx, psiHyx, psiHyz,
     )
 end
 
@@ -219,10 +219,10 @@ end
 
 
 function update_CPML_H!(model::Model2D)
-    (; field, Ax, Bx, Az, Bz, psiHyz, psiHyx) = model
-    (; dHyz, dHyx) = field
-    update_psi!(psiHyz, Az, Bz, dHyz; dim=2)
+    (; field, Ax, Bx, Az, Bz, psiHyx, psiHyz) = model
+    (; dHyx, dHyz) = field
     update_psi!(psiHyx, Ax, Bx, dHyx; dim=1)
+    update_psi!(psiHyz, Az, Bz, dHyz; dim=2)
     return nothing
 end
 
@@ -250,24 +250,35 @@ end
 # ******************************************************************************
 # 3D
 # ******************************************************************************
-struct Model3D{F, T, R, A, S} <: Model
+struct Model3D{F, S, T, R, A1, A2} <: Model
     field :: F
+    source :: S
     Nt :: Int
     dt :: T
     t :: R
-    mHx1 :: A
-    mHx2 :: A
-    mHy1 :: A
-    mHy2 :: A
-    mHz1 :: A
-    mHz2 :: A
-    mEx1 :: A
-    mEx2 :: A
-    mEy1 :: A
-    mEy2 :: A
-    mEz1 :: A
-    mEz2 :: A
-    source :: S
+    Mh :: T
+    Me :: T
+    Kx :: A1
+    Ax :: A1
+    Bx :: A1
+    Ky :: A1
+    Ay :: A1
+    By :: A1
+    Kz :: A1
+    Az :: A1
+    Bz :: A1
+    psiExy :: A2
+    psiExz :: A2
+    psiEyx :: A2
+    psiEyz :: A2
+    psiEzx :: A2
+    psiEzy :: A2
+    psiHxy :: A2
+    psiHxz :: A2
+    psiHyx :: A2
+    psiHyz :: A2
+    psiHzx :: A2
+    psiHzy :: A2
 end
 
 @adapt_structure Model3D
@@ -289,135 +300,77 @@ function Model(
     Nt = ceil(Int, tmax / dt)
     t = range(0, tmax, Nt)
 
-    sx, sy, sz = pml(grid, pml_box)
-    @. sx *= 1 / (2*dt)
-    @. sy *= 1 / (2*dt)
-    @. sz *= 1 / (2*dt)
+    Kx, Ax, Bx = pml(x, pml_box[1:2], dt)
+    Ky, Ay, By = pml(y, pml_box[3:4], dt)
+    Kz, Az, Bz = pml(z, pml_box[5:6], dt)
 
-    if isnothing(permittivity)
-        eps = 1
-        esigma = 0
-    else
-        tmp = [permittivity(x[ix], y[iy], z[iz]) for ix=1:Nx, iy=1:Ny, iz=1:Nz]
-        if smooth_interfaces
-            tmp = moving_average(tmp, 2)
-        end
-        eps = @. real(tmp)
-        esigma = @. EPS0 * w0 * imag(tmp)
-    end
-    if isnothing(permeability)
-        mu = 1
-        msigma = 0
-    else
-        tmp = [permeability(x[ix], y[iy], z[iz]) for ix=1:Nx, iy=1:Ny, iz=1:Nz]
-        if smooth_interfaces
-            tmp = moving_average(tmp, 2)
-        end
-        mu = @. real(tmp)
-        msigma = @. MU0 * w0 * imag(tmp)
-    end
+    Mh = dt / MU0
+    Me = dt / EPS0
 
-    # update coefficients:
-    mHx0 = @. (sy + sz + msigma / (MU0 * mu)) * dt / 2 +
-              (sy * sz + sx * msigma / (MU0 * mu)) * dt^2 / 4
-    mHx1 = @. (1 - mHx0) / (1 + mHx0)
-    mHx2 = @. -dt / (MU0 * mu) / (1 + mHx0)
-
-    mHy0 = @. (sx + sz + msigma / (MU0 * mu)) * dt / 2 +
-              (sx * sz + sy * msigma / (MU0 * mu)) * dt^2 / 4
-    mHy1 = @. (1 - mHy0) / (1 + mHy0)
-    mHy2 = @. -dt / (MU0 * mu) / (1 + mHy0)
-
-    mHz0 = @. (sx + sy + msigma / (MU0 * mu)) * dt / 2 +
-              (sx * sy + sz * msigma / (MU0 * mu)) * dt^2 / 4
-    mHz1 = @. (1 - mHz0) / (1 + mHz0)
-    mHz2 = @. -dt / (MU0 * mu) / (1 + mHz0)
-
-    mEx0 = @. (sy + sz + esigma / (EPS0 * eps)) * dt / 2 +
-              (sy * sz + sx * esigma / (EPS0 * eps)) * dt^2 / 4
-    mEx1 = @. (1 - mEx0) / (1 + mEx0)
-    mEx2 = @. dt / (EPS0 * eps) / (1 + mEx0)
-
-    mEy0 = @. (sx + sz + esigma / (EPS0 * eps)) * dt / 2 +
-              (sx * sz + sy * esigma / (EPS0 * eps)) * dt^2 / 4
-    mEy1 = @. (1 - mEy0) / (1 + mEy0)
-    mEy2 = @. dt / (EPS0 * eps) / (1 + mEy0)
-
-    mEz0 = @. (sx + sy + esigma / (EPS0 * eps)) * dt / 2 +
-              (sx * sy + sz * esigma / (EPS0 * eps)) * dt^2 / 4
-    mEz1 = @. (1 - mEz0) / (1 + mEz0)
-    mEz2 = @. dt / (EPS0 * eps) / (1 + mEz0)
+    psiExy, psiExz, psiEyx, psiEyz, psiEzx, psiEzy = (zeros(Nx,Ny,Nz) for i=1:6)
+    psiHxy, psiHxz, psiHyx, psiHyz, psiHzx, psiHzy = (zeros(Nx,Ny,Nz) for i=1:6)
 
     return Model3D(
-        field, Nt, dt, t,
-        mHx1, mHx2, mHy1, mHy2, mHz1, mHz2, mEx1, mEx2, mEy1, mEy2, mEz1, mEz2,
-        source,
+        field, source, Nt, dt, t, Mh, Me, Kx, Ax, Bx, Ky, Ay, By, Kz, Az, Bz,
+        psiExy, psiExz, psiEyx, psiEyz, psiEzx, psiEzy,
+        psiHxy, psiHxz, psiHyx, psiHyz, psiHzx, psiHzy,
     )
+    return nothing
+end
+
+
+function update_CPML_E!(model::Model3D)
+    (; field, Ax, Bx, Ay, By, Az, Bz) = model
+    (; psiExy, psiExz, psiEyx, psiEyz, psiEzx, psiEzy) = model
+    (; dExy, dExz, dEyx, dEyz, dEzx, dEzy) = field
+    update_psi!(psiExy, Ay, By, dExy; dim=2)
+    update_psi!(psiExz, Az, Bz, dExz; dim=3)
+    update_psi!(psiEyx, Ax, Bx, dEyx, dim=1)
+    update_psi!(psiEyz, Az, Bz, dEyz; dim=3)
+    update_psi!(psiEzx, Ax, Bx, dEzx; dim=1)
+    update_psi!(psiEzy, Ay, By, dEzy; dim=2)
+    return nothing
+end
+
+
+function update_CPML_H!(model::Model3D)
+    (; field, Ax, Bx, Ay, By, Az, Bz) = model
+    (; psiHxy, psiHxz, psiHyx, psiHyz, psiHzx, psiHzy) = model
+    (; dHxy, dHxz, dHyx, dHyz, dHzx, dHzy) = field
+    update_psi!(psiHxy, Ay, By, dHxy; dim=2)
+    update_psi!(psiHxz, Az, Bz, dHxz; dim=3)
+    update_psi!(psiHyx, Ax, Bx, dHyx, dim=1)
+    update_psi!(psiHyz, Az, Bz, dHyz; dim=3)
+    update_psi!(psiHzx, Ax, Bx, dHzx; dim=1)
+    update_psi!(psiHzy, Ay, By, dHzy; dim=2)
+    return nothing
 end
 
 
 function update_H!(model::Model3D)
-    (; field) = model
-    (; mHx1, mHx2, mHy1, mHy2, mHz1, mHz2) = model
-    (; Hx, Hy, Hz, CEx, CEy, CEz) = field
-    @. Hx = mHx1 * Hx + mHx2 * CEx
-    @. Hy = mHy1 * Hy + mHy2 * CEy
-    @. Hz = mHz1 * Hz + mHz2 * CEz
+    (; field, Mh, Kx, Ky, Kz) = model
+    (; psiExy, psiExz, psiEyx, psiEyz, psiEzx, psiEzy) = model
+    (; Hx, Hy, Hz, dExy, dExz, dEyx, dEyz, dEzx, dEzy) = field
+    @. Hx = Hx - Mh * (dEzy - dEyz) - Mh * (psiEzy - psiEyz)
+    @. Hy = Hy - Mh * (dExz - dEzx) - Mh * (psiExz - psiEzx)
+    @. Hz = Hz - Mh * (dEyx - dExy) - Mh * (psiEyx - psiExy)
+    # @. Hx = Hx - Mh * (dEzy / Ky - dEyz / Kz) - Mh * (psiEzy - psiEyz)
+    # @. Hy = Hy - Mh * (dExz / Kz - dEzx / Kx) - Mh * (psiExz - psiEzx)
+    # @. Hz = Hz - Mh * (dEyx / Kx - dExy / Ky) - Mh * (psiEyx - psiExy)
     return nothing
 end
 
 
 function update_E!(model::Model3D)
-    (; field) = model
-    (; mEx1, mEx2, mEy1, mEy2, mEz1, mEz2) = model
-    (; Ex, Ey, Ez, CHx, CHy, CHz) = field
-    @. Ex = mEx1 * Ex + mEx2 * CHx
-    @. Ey = mEy1 * Ey + mEy2 * CHy
-    @. Ez = mEz1 * Ez + mEz2 * CHz
-    return nothing
-end
-
-
-function step!(model::Model3D, it)
-    (; field, source, t) = model
-
-    # tfsf source:
-    # (; mHy2, mEx2, Esrc, Hsrc) = model
-    # (; grid, Hy, Ex) = field
-    # (; dz, z) = grid
-    # zsrc = 0
-    # izsrc = searchsortedfirst(z, zsrc)
-
-    @timeit "curl_E" begin
-        curl_E!(field)
-        synchronize()
-    end
-    @timeit "update_H" begin
-        update_H!(model)
-        synchronize()
-    end
-    # Esrc = source(t[it])
-    # Hy[izsrc-1] -= mHy2[izsrc-1]/dz * Esrc   # tfsf source
-
-    @timeit "curl_H" begin
-        curl_H!(field)
-        synchronize()
-    end
-    @timeit "update_E" begin
-        update_E!(model)
-        synchronize()
-    end
-    # n = sqrt(eps * mu)
-    # A = sqrt(eps / mu)
-    # td = n * dz / (2 * C0) + dt / 2
-    # Hsrc = A * source(t[it] + td)
-    # Ex[izsrc] += mEx2[izsrc]/dz * Hsrc   # tfsf source
-
-    @timeit "add_source" begin
-        add_source!(field, source, t[it])  # additive source
-        synchronize()
-    end
-
+    (; field, Me, Kx, Ky, Kz) = model
+    (; psiHxy, psiHxz, psiHyx, psiHyz, psiHzx, psiHzy) = model
+    (; Ex, Ey, Ez, dHxy, dHxz, dHyx, dHyz, dHzx, dHzy) = field
+    @. Ex = Ex + Me * (dHzy - dHyz) + Me * (psiHzy - psiHyz)
+    @. Ey = Ey + Me * (dHxz - dHzx) + Me * (psiHxz - psiHzx)
+    @. Ez = Ez + Me * (dHyx - dHxy) + Me * (psiHyx - psiHxy)
+    # @. Ex = Ex + Me * (dHzy / Ky - dHyz / Kz) + Me * (psiHzy - psiHyz)
+    # @. Ey = Ey + Me * (dHxz / Kz - dHzx / Kx) + Me * (psiHxz - psiHzx)
+    # @. Ez = Ez + Me * (dHyx / Kx - dHxy / Ky) + Me * (psiHyx - psiHxy)
     return nothing
 end
 
