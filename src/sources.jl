7e0ccf7522f59502d5eafe6748dc02b950b17580
iamfedoroff <iamfedoroff@gmail.com>
2023-05-05 18:22:27 +0300

    Optimize memory consumption in source injection functions

--- src/sources.jl
+++ src/sources.jl
@@ -1,18 +1,19 @@
-struct Source{A, F, P, S}
+struct Source{C, S, A, F, P}
+    isrc :: C
+    component :: S
     Amp :: A
     Phi :: A
     waveform :: F
     p :: P
-    component :: S
 end
 
 @adapt_structure Source
 
 
 function add_source!(field, source, t)
-    (; Amp, Phi, waveform, p, component) = source
+    (; isrc, component, Amp, Phi, waveform, p) = source
     F = getfield(field, component)
-    @. F += Amp * waveform(t - Phi, (p,))
+    @views @. F[isrc] += Amp * waveform(t - Phi, (p,))
     return nothing
 end
 
@@ -22,16 +23,17 @@ function PointSource(
     grid::Grid1D;
     position, component, frequency, amplitude=1, phase=0, waveform, p=(),
 )
-    (; Nz, z) = grid
+    (; z) = grid
 
     zsrc = position
     izsrc = searchsortedfirst(z, zsrc)
+    isrc = CartesianIndices((izsrc:izsrc,))
 
-    Amp, Phi = zeros(Nz), zeros(Nz)
-    Amp[izsrc] = amplitude
-    Phi[izsrc] = phase / frequency
+    Amp = amplitude
+    Phi = phase / frequency
+    Amp, Phi = promote(Amp, Phi)
 
-    return Source(Amp, Phi, waveform, p, component)
+    return Source(isrc, component, Amp, Phi, waveform, p)
 end
 
 
@@ -39,17 +41,18 @@ function PointSource(
     grid::Grid2D;
     position, component, frequency, amplitude=1, phase=0, waveform, p=(),
 )
-    (; Nx, Nz, x, z) = grid
+    (; x, z) = grid
 
     xsrc, zsrc = position
     ixsrc = searchsortedfirst(x, xsrc)
     izsrc = searchsortedfirst(z, zsrc)
+    isrc = CartesianIndices((ixsrc:ixsrc, izsrc:izsrc))
 
-    Amp, Phi = zeros(Nx,Nz), zeros(Nx,Nz)
-    Amp[ixsrc,izsrc] = amplitude
-    Phi[ixsrc,izsrc] = phase / frequency
+    Amp = amplitude
+    Phi = phase / frequency
+    Amp, Phi = promote(Amp, Phi)
 
-    return Source(Amp, Phi, waveform, p, component)
+    return Source(isrc, component, Amp, Phi, waveform, p)
 end
 
 
@@ -57,18 +60,19 @@ function PointSource(
     grid::Grid3D;
     position, component, frequency, amplitude=1, phase=0, waveform, p=(),
 )
-    (; Nx, Ny, Nz, x, y, z) = grid
+    (; x, y, z) = grid
 
     xsrc, ysrc, zsrc = position
     ixsrc = searchsortedfirst(x, xsrc)
     iysrc = searchsortedfirst(y, ysrc)
     izsrc = searchsortedfirst(z, zsrc)
+    isrc = CartesianIndices((ixsrc:ixsrc, iysrc:iysrc, izsrc:izsrc))
 
-    Amp, Phi = zeros(Nx,Ny,Nz), zeros(Nx,Ny,Nz)
-    Amp[ixsrc,iysrc,izsrc] = amplitude
-    Phi[ixsrc,iysrc,izsrc] = phase / frequency
+    Amp = amplitude
+    Phi = phase / frequency
+    Amp, Phi = promote(Amp, Phi)
 
-    return Source(Amp, Phi, waveform, p, component)
+    return Source(isrc, component, Amp, Phi, waveform, p)
 end
 
 
@@ -77,18 +81,19 @@ function LineSource(
     grid::Grid2D;
     position, component, frequency, amplitude, phase, waveform, p=(),
 )
-    (; Nx, Nz, x, z) = grid
+    (; Nx, x, z) = grid
 
     zsrc = position
     izsrc = searchsortedfirst(z, zsrc)
+    isrc = CartesianIndices((1:Nx, izsrc:izsrc))
 
-    Amp, Phi = zeros(Nx,Nz), zeros(Nx,Nz)
+    Amp, Phi = zeros(Nx), zeros(Nx)
     for ix=1:Nx
-        Amp[ix,izsrc] = amplitude(x[ix])
-        Phi[ix,izsrc] = phase(x[ix]) / frequency
+        Amp[ix] = amplitude(x[ix])
+        Phi[ix] = phase(x[ix]) / frequency
     end
 
-    return Source(Amp, Phi, waveform, p, component)
+    return Source(isrc, component, Amp, Phi, waveform, p)
 end
 
 
@@ -97,16 +102,17 @@ function PlaneSource(
     grid::Grid3D;
     position, component, frequency, amplitude, phase, waveform, p=(),
 )
-    (; Nx, Ny, Nz, x, y, z) = grid
+    (; Nx, Ny, x, y, z) = grid
 
     zsrc = position
     izsrc = searchsortedfirst(z, zsrc)
+    isrc = CartesianIndices((1:Nx, 1:Ny, izsrc:izsrc))
 
-    Amp, Phi = zeros(Nx,Ny,Nz), zeros(Nx,Ny,Nz)
+    Amp, Phi = zeros(Nx,Ny), zeros(Nx,Ny)
     for iy=1:Ny, ix=1:Nx
-        Amp[ix,iy,izsrc] = amplitude(x[ix], y[iy])
-        Phi[ix,iy,izsrc] = phase(x[ix], y[iy]) / frequency
+        Amp[ix,iy] = amplitude(x[ix], y[iy])
+        Phi[ix,iy] = phase(x[ix], y[iy]) / frequency
     end
 
-    return Source(Amp, Phi, waveform, p, component)
+    return Source(isrc, component, Amp, Phi, waveform, p)
 end
